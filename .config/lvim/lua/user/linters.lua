a6a3e8c58b725cb5c6b54b040b817ea161bb0f91
Anders Njøs Slinde <anders.slinde@gmail.com>
2023-04-14 12:55:02 +0200

    Fix quite a bit of stuff

--- .config/lvim/lua/user/linters.lua
+++ .config/lvim/lua/user/linters.lua
@@ -1,14 +1,14 @@
 local M = {}
 
 M.config = function()
-	-- set a formatter, this will override the language server formatting capabilities (if it exists)
-	local linters = require("lvim.lsp.null-ls.linters")
-	linters.setup({
-		{
-			exe = "eslint_d",
-			---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
-			filetypes = { "javascript", "typescript", "typescriptreact" },
-		},
-	})
+  -- set a formatter, this will override the language server formatting capabilities (if it exists)
+  local linters = require("lvim.lsp.null-ls.linters")
+  linters.setup({
+    {
+      exe = "eslint_d",
+      ---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
+      filetypes = { "javascript", "typescript", "typescriptreact" },
+    },
+  })
 end
 return M
