b6ca536496710ea79d36102c5346cfbf9212d997
Anders Njøs Slinde <anders.slinde@gmail.com>
2023-08-16 12:03:40 +0200

    Remove l

--- .config/nvim/lua/plugins/whichkey.lua
+++ .config/nvim/lua/plugins/whichkey.lua
@@ -1,12 +1,6 @@
 return {
   {
     "folke/which-key.nvim",
-    opts = function()
-      require("which-key").register({
-        ["<leader>l"] = {
-          name = "+language",
-        },
-      })
-    end,
+    opts = function() end,
   },
 }
