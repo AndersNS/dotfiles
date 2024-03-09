76a10e801e7ec6472dcb04fb17ab7fc52b01f1a7
Anders Njøs Slinde <anders.slinde@gmail.com>
2024-03-09 11:42:46 +0100

    Markdown preview

--- .config/nvim/lua/plugins/markdown.lua
+++ .config/nvim/lua/plugins/markdown.lua
@@ -10,4 +10,13 @@ return {
       end,
     },
   },
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    build = "cd app && pnpm install",
+    init = function()
+      vim.g.mkdp_filetypes = { "markdown" }
+    end,
+    ft = { "markdown" },
+  },
 }
