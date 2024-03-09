b300a7071be8a678d360cfb590dc1e15401e7be7
Anders Njøs Slinde <anders.slinde@gmail.com>
2024-03-09 11:44:09 +0100

    Some bufferline changes

---
+++ .config/nvim/lua/plugins/bufferline.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "akinsho/bufferline.nvim",
+    opts = {
+      options = {
+        max_name_length = 30,
+        tab_size = 30,
+        truncate_names = true, -- whether or not tab names should be truncated
+      },
+    },
+  },
+}
