2d544dcb947b2a9e2bfba9526f7c3edbe8d59ad8
Anders Njøs Slinde <anders.slinde@gmail.com>
2024-06-03 08:19:06 +0200

    Go back to lazyvims rust setup

--- .config/nvim/lua/plugins/rust-tools.lua
+++ .config/nvim/lua/plugins/rust-tools.lua
@@ -1,3 +1,4 @@
+return {}
 -- vim.g.rustaceanvim = {
 --   -- Plugin configuration
 --   tools = {},
@@ -25,45 +26,45 @@
 --   dap = {},
 -- }
 
-return {
-  -- {
-  --   "mrcjkb/rustaceanvim",
-  --   version = "^3", -- Recommended
-  --   ft = { "rust" },
-  -- },
-  {
-    "simrat39/rust-tools.nvim",
-    lazy = true,
-    enabled = false,
-    ft = { "rust" },
-    opts = {
-      tools = {
-        on_initialized = function()
-          vim.cmd([[
-              augroup RustLSP
-                autocmd CursorHold                      *.rs silent! lua vim.lsp.buf.document_highlight()
-                autocmd CursorMoved,InsertEnter         *.rs silent! lua vim.lsp.buf.clear_references()
-                autocmd BufEnter,CursorHold,InsertLeave *.rs silent! lua vim.lsp.codelens.refresh()
-              augroup END
-            ]])
-
-          require("rust-tools").inlay_hints.disable()
-
-          require("which-key").register({
-            ["<leader>r"] = {
-              name = "+rust",
-            },
-          })
-          vim.keymap.set("n", "<leader>ci", ":RustEnableInlayHints<CR>", { silent = true, desc = "Enable hints" })
-          vim.keymap.set("n", "<leader>cI", ":RustDisableInlayHints<CR>", { silent = true, desc = "Disable hints" })
-          vim.keymap.set("n", "<leader>cR", ":RustRun<CR>", { desc = "Run" })
-          vim.keymap.set("n", "<leader>cA", ":RustRunnables<CR>", { desc = "Runnables" })
-          vim.keymap.set("n", "<leader>cD", ":RustDebuggables<CR>", { desc = "Debuggables" })
-          vim.keymap.set("n", "<leader>ck", ":RustMoveItemUp<CR>", { desc = "Move item up" })
-          vim.keymap.set("n", "<leader>cj", ":RustMoveItemDown<CR>", { desc = "Move item down" })
-          vim.keymap.set("n", "<leader>ch", ":RustHoverActions<CR>", { desc = "Hover Actions (help)" })
-        end,
-      },
-    },
-  },
-}
+-- return {
+-- {
+--   "mrcjkb/rustaceanvim",
+--   version = "^3", -- Recommended
+--   ft = { "rust" },
+-- },
+-- {
+--   "simrat39/rust-tools.nvim",
+--   lazy = true,
+--   enabled = false,
+--   ft = { "rust" },
+--   opts = {
+--     tools = {
+--       on_initialized = function()
+--         vim.cmd([[
+--             augroup RustLSP
+--               autocmd CursorHold                      *.rs silent! lua vim.lsp.buf.document_highlight()
+--               autocmd CursorMoved,InsertEnter         *.rs silent! lua vim.lsp.buf.clear_references()
+--               autocmd BufEnter,CursorHold,InsertLeave *.rs silent! lua vim.lsp.codelens.refresh()
+--             augroup END
+--           ]])
+--
+--         require("rust-tools").inlay_hints.disable()
+--
+--         require("which-key").register({
+--           ["<leader>r"] = {
+--             name = "+rust",
+--           },
+--         })
+--         vim.keymap.set("n", "<leader>ci", ":RustEnableInlayHints<CR>", { silent = true, desc = "Enable hints" })
+--         vim.keymap.set("n", "<leader>cI", ":RustDisableInlayHints<CR>", { silent = true, desc = "Disable hints" })
+--         vim.keymap.set("n", "<leader>cR", ":RustRun<CR>", { desc = "Run" })
+--         vim.keymap.set("n", "<leader>cA", ":RustRunnables<CR>", { desc = "Runnables" })
+--         vim.keymap.set("n", "<leader>cD", ":RustDebuggables<CR>", { desc = "Debuggables" })
+--         vim.keymap.set("n", "<leader>ck", ":RustMoveItemUp<CR>", { desc = "Move item up" })
+--         vim.keymap.set("n", "<leader>cj", ":RustMoveItemDown<CR>", { desc = "Move item down" })
+--         vim.keymap.set("n", "<leader>ch", ":RustHoverActions<CR>", { desc = "Hover Actions (help)" })
+--       end,
+--     },
+--   },
+-- },
+-- }
