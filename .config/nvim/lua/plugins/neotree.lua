0567190237e5c472182cda94f442ff79e026c9fa
Anders Njøs Slinde <anders.slinde@gmail.com>
2024-09-25 07:43:50 +0200

    Right side is right

--- .config/nvim/lua/plugins/neotree.lua
+++ .config/nvim/lua/plugins/neotree.lua
@@ -2,6 +2,9 @@ return {
   {
     "nvim-neo-tree/neo-tree.nvim",
     opts = {
+      window = {
+        position = "right",
+      },
       filesystem = {
         filtered_items = {
           visible = false,
