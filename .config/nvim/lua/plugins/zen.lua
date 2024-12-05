e11d544638641486a99f231379b154f461d010d7
Anders Njøs Slinde <anders.slinde@gmail.com>
2024-12-05 14:44:05 +0100

    Moved alias and updated lazy

--- .config/nvim/lua/plugins/zen.lua
+++ .config/nvim/lua/plugins/zen.lua
@@ -1,12 +1,13 @@
 return {
   {
     "folke/twilight.nvim",
+    enabled = false,
     opts = {
       -- your configuration comes here
       -- or leave it empty to use the default settings
       -- refer to the configuration section below
       alpha = 0.8,
-      context = 50,
+      context = 100,
       treesitter = true,
     },
   },
