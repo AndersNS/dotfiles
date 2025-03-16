612994bb13cc1b6fca0f01acb7836548b0e32f5e
Anders Njøs Slinde <anders.slinde@gmail.com>
2025-03-16 23:54:47 +0100

    Disable some markdown stuff

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -1,7 +1,14 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
-
+vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+vim.api.nvim_create_autocmd("FileType", {
+  group = vim.api.nvim_create_augroup("lazyvim_user_markdown", { clear = true }),
+  pattern = { "gitcommit", "markdown" },
+  callback = function()
+    vim.opt_local.wrap = true
+  end,
+})
 -- Very cool hack to fix omnisharp
 vim.api.nvim_create_autocmd("LspAttach", {
   desc = "Fix startup error by disabling semantic tokens for omnisharp",
