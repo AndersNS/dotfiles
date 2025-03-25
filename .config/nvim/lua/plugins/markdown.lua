6d87329b965aeb450a8bc44d186d9d56059dab36
Anders Njøs Slinde <anders.slinde@gmail.com>
2025-03-25 22:24:48 +0100

    Some markdown shenaningans

--- .config/nvim/lua/plugins/markdown.lua
+++ .config/nvim/lua/plugins/markdown.lua
@@ -49,12 +49,104 @@ return {
         --   'highlight':       Highlight for the 'rendered' icon
         --   'scope_highlight': Highlight for item associated with custom checkbox
         custom = {
-          todo = { raw = "[-]", rendered = "󰥔 ", highlight = "RenderMarkdownTodo", scope_highlight = nil },
+          later = { raw = "[-]", rendered = "󰥔 ", highlight = "RenderMarkdownTodo", scope_highlight = nil },
+          moved = { raw = "[>]", rendered = "󰧚 ", highlight = "RenderMarkdownTodo", scope_highlight = nil },
+          important = {
+            raw = "[!]",
+            rendered = " ",
+            highlight = "RenderMarkdownTodo",
+            scope_highlight = "RenderMarkdownInfo",
+          },
+          deleted = {
+            raw = "[~]",
+            rendered = "󰮉 ",
+            highlight = "RenderMarkdownTodo",
+            scope_highlight = "RenderMarkdownHtmlComment",
+          },
         },
       },
       bullet = {
         left_pad = 1,
         right_pad = 1,
+      },
+      heading = {
+        -- Turn on / off heading icon & background rendering.
+        enabled = true,
+        -- Additional modes to render headings.
+        render_modes = true,
+        -- Turn on / off any sign column related rendering.
+        sign = false,
+        -- Replaces '#+' of 'atx_h._marker'.
+        -- Output is evaluated depending on the type.
+        -- | function | `value(context)`              |
+        -- | string[] | `cycle(value, context.level)` |
+        -- icons = {},
+        -- Determines how icons fill the available space.
+        -- | right   | '#'s are concealed and icon is appended to right side                          |
+        -- | inline  | '#'s are concealed and icon is inlined on left side                            |
+        -- | overlay | icon is left padded with spaces and inserted on left hiding any additional '#' |
+        position = "overlay",
+        -- Added to the sign column if enabled.
+        -- Output is evaluated by `cycle(value, context.level)`.
+        -- signs = { "󰫎 " },
+        -- Width of the heading background.
+        -- | block | width of the heading text |
+        -- | full  | full width of the window  |
+        -- Can also be a list of the above values evaluated by `clamp(value, context.level)`.
+        width = "block",
+        -- Amount of margin to add to the left of headings.
+        -- Margin available space is computed after accounting for padding.
+        -- If a float < 1 is provided it is treated as a percentage of available window space.
+        -- Can also be a list of numbers evaluated by `clamp(value, context.level)`.
+        left_margin = 0,
+        -- Amount of padding to add to the left of headings.
+        -- Output is evaluated using the same logic as 'left_margin'.
+        left_pad = 0,
+        -- Amount of padding to add to the right of headings when width is 'block'.
+        -- Output is evaluated using the same logic as 'left_margin'.
+        right_pad = 0,
+        -- Minimum width to use for headings when width is 'block'.
+        -- Can also be a list of integers evaluated by `clamp(value, context.level)`.
+        min_width = 0,
+        -- Determines if a border is added above and below headings.
+        -- Can also be a list of booleans evaluated by `clamp(value, context.level)`.
+        border = false,
+        -- Always use virtual lines for heading borders instead of attempting to use empty lines.
+        border_virtual = false,
+        -- Highlight the start of the border using the foreground highlight.
+        border_prefix = false,
+        -- Used above heading for border.
+        above = "▄",
+        -- Used below heading for border.
+        below = "▀",
+        -- Highlight for the heading icon and extends through the entire line.
+        -- Output is evaluated by `clamp(value, context.level)`.
+        backgrounds = {
+          "RenderMarkdownH1Bg",
+          "RenderMarkdownH2Bg",
+          "RenderMarkdownH3Bg",
+          "RenderMarkdownH4Bg",
+          "RenderMarkdownH5Bg",
+          "RenderMarkdownH6Bg",
+        },
+        -- Highlight for the heading and sign icons.
+        -- Output is evaluated using the same logic as 'backgrounds'.
+        foregrounds = {
+          "RenderMarkdownH1",
+          "RenderMarkdownH2",
+          "RenderMarkdownH3",
+          "RenderMarkdownH4",
+          "RenderMarkdownH5",
+          "RenderMarkdownH6",
+        },
+        -- Define custom heading patterns which allow you to override various properties based on
+        -- the contents of a heading.
+        -- The key is for healthcheck and to allow users to change its values, value type below.
+        -- | pattern    | matched against the heading text @see :h lua-pattern |
+        -- | icon       | optional override for the icon                       |
+        -- | background | optional override for the background                 |
+        -- | foreground | optional override for the foreground                 |
+        custom = {},
       },
     },
   },
