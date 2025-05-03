6eb88d09d1cf40766f156b403ea573f84fc562c4
Anders Njøs Slinde <anders.slinde@gmail.com>
2025-05-03 08:35:45 +0200

    back to basics

--- .config/nvim/lua/plugins/multicursor.lua
+++ .config/nvim/lua/plugins/multicursor.lua
@@ -1,40 +1,40 @@
 return {
-  -- { "mg979/vim-visual-multi" },
-  {
-    "brenton-leighton/multiple-cursors.nvim",
-    version = "*", -- Use the latest tagged version
-    lazy = false,
-    opts = {}, -- This causes the plugin setup function to be called
-    keys = {
-      { "<C-j>", "<Cmd>MultipleCursorsAddDown<CR>", mode = { "n", "x" }, desc = "Add cursor and move down" },
-      { "<C-k>", "<Cmd>MultipleCursorsAddUp<CR>", mode = { "n", "x" }, desc = "Add cursor and move up" },
-      { "<C-Up>", "<Cmd>MultipleCursorsAddUp<CR>", mode = { "n", "i", "x" }, desc = "Add cursor and move up" },
-      { "<C-Down>", "<Cmd>MultipleCursorsAddDown<CR>", mode = { "n", "i", "x" }, desc = "Add cursor and move down" },
-
-      {
-        "<Leader>mm",
-        "<Cmd>MultipleCursorsAddVisualArea<CR>",
-        mode = { "x" },
-        desc = "Add cursors to the lines of the visual area",
-      },
-
-      { "<Leader>ma", "<Cmd>MultipleCursorsAddMatches<CR>", mode = { "n", "x" }, desc = "Add cursors to cword" },
-      {
-        "<Leader>mA",
-        "<Cmd>MultipleCursorsAddMatchesV<CR>",
-        mode = { "n", "x" },
-        desc = "Add cursors to cword in previous area",
-      },
-
-      {
-        "<Leader>md",
-        "<Cmd>MultipleCursorsAddJumpNextMatch<CR>",
-        mode = { "n", "x" },
-        desc = "Add cursor and jump to next cword",
-      },
-      { "<Leader>mD", "<Cmd>MultipleCursorsJumpNextMatch<CR>", mode = { "n", "x" }, desc = "Jump to next cword" },
-
-      { "<Leader>ml", "<Cmd>MultipleCursorsLock<CR>", mode = { "n", "x" }, desc = "Lock virtual cursors" },
-    },
-  },
+  { "mg979/vim-visual-multi" },
+  -- {
+  --   "brenton-leighton/multiple-cursors.nvim",
+  --   version = "*", -- Use the latest tagged version
+  --   lazy = false,
+  --   opts = {}, -- This causes the plugin setup function to be called
+  --   keys = {
+  --     { "<C-j>", "<Cmd>MultipleCursorsAddDown<CR>", mode = { "n", "x" }, desc = "Add cursor and move down" },
+  --     { "<C-k>", "<Cmd>MultipleCursorsAddUp<CR>", mode = { "n", "x" }, desc = "Add cursor and move up" },
+  --     { "<C-Up>", "<Cmd>MultipleCursorsAddUp<CR>", mode = { "n", "i", "x" }, desc = "Add cursor and move up" },
+  --     { "<C-Down>", "<Cmd>MultipleCursorsAddDown<CR>", mode = { "n", "i", "x" }, desc = "Add cursor and move down" },
+  --
+  --     {
+  --       "<Leader>mm",
+  --       "<Cmd>MultipleCursorsAddVisualArea<CR>",
+  --       mode = { "x" },
+  --       desc = "Add cursors to the lines of the visual area",
+  --     },
+  --
+  --     { "<Leader>ma", "<Cmd>MultipleCursorsAddMatches<CR>", mode = { "n", "x" }, desc = "Add cursors to cword" },
+  --     {
+  --       "<Leader>mA",
+  --       "<Cmd>MultipleCursorsAddMatchesV<CR>",
+  --       mode = { "n", "x" },
+  --       desc = "Add cursors to cword in previous area",
+  --     },
+  --
+  --     {
+  --       "<Leader>md",
+  --       "<Cmd>MultipleCursorsAddJumpNextMatch<CR>",
+  --       mode = { "n", "x" },
+  --       desc = "Add cursor and jump to next cword",
+  --     },
+  --     { "<Leader>mD", "<Cmd>MultipleCursorsJumpNextMatch<CR>", mode = { "n", "x" }, desc = "Jump to next cword" },
+  --
+  --     { "<Leader>ml", "<Cmd>MultipleCursorsLock<CR>", mode = { "n", "x" }, desc = "Lock virtual cursors" },
+  --   },
+  -- },
 }
