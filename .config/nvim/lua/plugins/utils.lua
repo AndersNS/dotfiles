c8e44f0884852d94ad79b420a6a84a05e6dd0da7
Anders Njøs Slinde <anders.slinde@gmail.com>
2025-12-11 20:48:43 +0100

    Some small stuff

--- .config/nvim/lua/plugins/utils.lua
+++ .config/nvim/lua/plugins/utils.lua
@@ -18,6 +18,18 @@ return {
           -- vim.opt_local.colorcolumn = { 80 }
         end,
       },
+      file_panel = {
+        listing_style = "list", -- One of 'list' or 'tree'
+        tree_options = { -- Only applies when listing_style is 'tree'
+          flatten_dirs = true, -- Flatten dirs that only contain one single dir
+          folder_statuses = "only_folded", -- One of 'never', 'only_folded' or 'always'.
+        },
+        win_config = { -- See |diffview-config-win_config|
+          position = "left",
+          width = 50,
+          win_opts = {},
+        },
+      },
     },
   },
   {
