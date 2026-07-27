88a4c1bc22f52f178befb43c71e9e7953ae3a0b2
Anders Njøs Slinde <anders.slinde@gmail.com>
2026-07-27 09:43:47 +0200

    some roslyn attempts

--- .config/nvim/lua/plugins/roslyn.lua
+++ .config/nvim/lua/plugins/roslyn.lua
@@ -25,6 +25,12 @@ return {
     "nsidorenco/neotest-vstest",
   },
   {
+    "bosvik/roslyn-diagnostics.nvim",
+    -- lazy load on filetype
+    ft = { "cs", "fs" },
+    opts = {},
+  },
+  {
 
     "seblyng/roslyn.nvim",
     dependencies = {
@@ -154,7 +160,7 @@ return {
             csharp_enable_inlay_hints_for_implicit_object_creation = true,
             csharp_enable_inlay_hints_for_implicit_variable_types = true,
             csharp_enable_inlay_hints_for_lambda_parameter_types = true,
-            csharp_enable_inlay_hints_for_types = true,
+            csharp_enable_inlay_hints_for_types = false,
             dotnet_enable_inlay_hints_for_indexer_parameters = true,
             dotnet_enable_inlay_hints_for_literal_parameters = true,
             dotnet_enable_inlay_hints_for_object_creation_parameters = true,
@@ -165,14 +171,14 @@ return {
             dotnet_suppress_inlay_hints_for_parameters_that_match_method_intent = true,
           },
           ["csharp|code_lens"] = {
-            dotnet_enable_references_code_lens = true,
+            dotnet_enable_references_code_lens = false,
           },
           ["csharp|formatting"] = {
             dotnet_organize_imports_on_format = true,
           },
           ["csharp|background_analysis"] = {
             ["background_analysis.dotnet_analyzer_diagnostics_scope"] = "openFiles",
-            ["background_analysis.dotnet_compiler_diagnostics_scope"] = "openFiles",
+            ["background_analysis.dotnet_compiler_diagnostics_scope"] = "solution",
           },
         },
       })
